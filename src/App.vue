46486cf89ece9670ab66e7ddbe5ff737949d2518
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-01-31 21:11:24 +0300

    загрузил верстку

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    Hello Vue.JS
+    <!-- <h1>Сайт Путешествий, Ёпта</h1> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-export default {
-    
-}
+import IndexPage from "./views/IndexPage.vue";
+export default {};
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/style.css";
+import "./assets/css/bootstrap.min.css";
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import IndexPage from "../views/IndexPage.vue";
+import SinglePage from "../views/SinglePage.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: IndexPage,
+    },
+    {
+      path: "/single",
+      component: SinglePage,
+    },
+  ],
+});
+
+export default router;
